d4075ad2c03f6b8f7a56981d189d4953a5887876
Andrzej Chmielewski <andrzejchm@gmail.com>
2022-09-17 00:43:33 +0200

    update app name

--- project/__brick__/lib/main.dart
+++ project/__brick__/lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: unused-code
 import 'package:{{{app_package_name}}}/core/helpers.dart';
 import 'package:{{{app_package_name}}}/dependency_injection/app_component.dart';
-import 'package:{{{app_package_name}}}/{{{app_package_name}}}_app.dart';
+import 'package:{{{app_package_name}}}/{{{app_name_snake}}}_app.dart';
 
 /// flag modified by unit tests so that app's code can adapt to unit tests
 /// (i.e: disable animations in progress bars etc.)
