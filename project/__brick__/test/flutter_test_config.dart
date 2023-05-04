dc06a823a435e0c92d3dda001ceb32e34b477fc8
Andrzej Chmielewski <andrzejchm@gmail.com>
2023-05-04 14:54:42 +0200

    add     prepareAppForUnitTests() to test setUp methods

--- project/__brick__/test/flutter_test_config.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:async';
-
-import 'test_utils/test_utils.dart';
-
-Future<void> testExecutable(FutureOr<void> Function() testMain) async {
-  await prepareAppForUnitTests();
-  return testMain();
-}
